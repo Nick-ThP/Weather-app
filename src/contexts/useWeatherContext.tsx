4d98239430bbd979096da42444cb2a41c3e6bc62
Nicklas <95386272+Nick-ThP@users.noreply.github.com>
2023-07-06 00:30:37 +0200

    commit

--- src/contexts/useWeatherContext.tsx
+++ src/contexts/useWeatherContext.tsx
@@ -1,14 +1,18 @@
-import { ReactNode, createContext, useContext, useState } from "react"
+import { ReactNode, createContext, useContext, useEffect, useState } from "react"
 import useLocalStorage from "../hooks/useLocalStorage"
+import axios from "axios"
+import cityData from '../cityData.json'
 
 interface IWeatherContext {
 	weatherData: unknown,
+	loading: boolean,
 	city: string,
 	setCity: (value: string) => void
 }
 
 const WeatherContext = createContext<IWeatherContext>({
 	weatherData: null,
+	loading: false,
 	city: '',
 	setCity() {}
 })
@@ -26,13 +30,40 @@ export function useWeatherContext() {
 export function WeatherContextProvider({ children }: { children: ReactNode }) {
 	const [city, setCity] = useLocalStorage('city', 'Aalborg')
 	const [weatherData, setWeatherData] = useState<unknown | null>(null)
+	const [loading, setLoading] = useState(false)
 
-	// async function loadUserData() {
-	// 	const weatherData = await
-	// }
+	function createURL(arg: string) {
+		const lat = cityData[city as keyof typeof cityData].lat
+		const lon = cityData[city as keyof typeof cityData].lon
+		const apiKey = process.env.REACT_APP_API_KEY
+
+		return `https://api.openweathermap.org/data/2.5/${arg}?lat=${lat}&lon=${lon}&appid=${apiKey}`
+	}
+
+	async function executeApiRequest() {
+		setLoading(true)
+
+		try {
+			const response = await axios(createURL('forecast'))
+			setWeatherData(response)
+		}
+
+		catch {
+			console.error('API request failed')
+		}
+
+		finally {
+			setLoading(false)
+		}
+	}
+
+	useEffect(() => {
+		executeApiRequest()
+	}, [city])
 
 	const contextData: IWeatherContext = {
 		weatherData,
+		loading,
 		city,
 		setCity
 	}
@@ -41,5 +72,5 @@ export function WeatherContextProvider({ children }: { children: ReactNode }) {
 		<WeatherContext.Provider value={contextData}>
 			{children}
 		</WeatherContext.Provider>
-  	)
+	)
 }
