9a9146257b669e776f7bc583f1ded790388f5528
Nicklas <95386272+Nick-ThP@users.noreply.github.com>
2023-07-06 17:32:09 +0200

    update

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,25 @@
 import { useWeatherContext } from '../../contexts/useWeatherContext'
-import styles from './app.module.sass'
+import styles from './app.module.scss'
 
 function App() {
 	const contextData = useWeatherContext()
 
 	return (
-		<>
-			{contextData.error && (
-				<div className={styles.errorContainer}>{contextData.error}</div>
+		<div className={styles.appContainer}>
+			<div className={styles.title}>Weather App</div>
+			{contextData.error ? (
+				<div className={styles.errorMessage}>{contextData.error}</div>
+			) : (
+				<>
+					<div className={styles.search}></div>
+					<div className={styles.favorites}></div>
+					<div key="1" className={styles.col1}></div>
+					<div key="2" className={styles.col2}></div>
+					<div key="3" className={styles.col3}></div>
+					<div key="4" className={styles.col4}></div>
+				</>
 			)}
-
-
-		</>
+		</div>
 	)
 
 
