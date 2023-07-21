3416c79cdf085d74fee58842f38a025a730999d2
Nicklas <95386272+Nick-ThP@users.noreply.github.com>
2023-07-21 17:51:31 +0200

    done

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,12 @@
+import { useWeatherContext } from "../../../contexts/useWeatherContext"
+import styles from './error.module.scss'
+
+export default function Error() {
+	const { error } = useWeatherContext()
+
+	return (
+		<div className={styles.container}>
+			{error}
+		</div>
+	)
+}
